bf2648bf4a3b64f3bdf1266a2e0397e23e2d1ff8
yogeshtiwari12 <yogesh_tiwari@gla.ac.in>
2025-02-13 23:44:37 +0530

    Updated backend and dependencies

--- backend.js
+++ backend.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://todo-app-backend-qibv.onrender.com',
     credentials: true,
   }));
 
