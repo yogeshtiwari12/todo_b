e7c737f441c4f4ee4d2d4d61addb625fefe77a93
Yogesh Tiwari <119746250+yogeshtiwari12@users.noreply.github.com>
2025-02-15 01:35:52 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import { allusers, allusertodo, login, logout, signup, valid_user } from '../methods/user.js';
-import { verifytoken } from '../auth/auth.js';
+import { isadmin, verifytoken } from '../auth/auth.js';
 import { usertodos } from '../methods/todo.js';
 
 
 const routes = express.Router();
 routes.put('/signup',signup)
 routes.post('/login',login)
-routes.post('/logout',verifytoken,logout)
+routes.post('/logout',logout)
 routes.get('/getauthuser',verifytoken,valid_user)
-routes.get('/allusertodo/:id',verifytoken,allusertodo)
-routes.get('/allusers',verifytoken,allusers)
+routes.get('/allusertodo/:id',verifytoken,isadmin("admin"),allusertodo)
+routes.get('/allusers',verifytoken,isadmin("admin"),allusers)
 
 
 export default routes;
